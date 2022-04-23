4ecf32d55e3c72298173a9d57f316994b0bbb92b
GiteeG <214142603@qq.com>
2022-04-23 21:46:23 +0800

    vue practise

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
+  },
+  data (){
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Learn JavaScript',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Learn Vue',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Build something awesome',
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.5;
 }
 </style>
